11ad289501998ae11d1f81fd33ab218ed7e9c1ba
Chandrakethan <90309231+chandrakethan27@users.noreply.github.com>
2024-01-30 21:36:49 +0530

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,14 +1,20 @@
 # Initial array to be sorted
-arr = [7, 2, 5, 1, 9, 4]
+arr = [7, 2, 5, 1, 9, 4, 4]
 
 # Outer loop: Iterate through each element of the array
 for i in range(len(arr)):
-    # Inner loop: Compare the current element with all other elements in the array
-    for j in range(len(arr)):
-        # Check if the current element is smaller than the element being compared
-        if arr[i] < arr[j]:
+    # Inner loop: Iterate through each pair of adjacent elements
+    for j in range(len(arr)-1-i):
+        # Compare the current element with the next element
+        if arr[j] > arr[j+1]:
             # Swap the elements if they are in the wrong order
-            arr[i], arr[j] = arr[j], arr[i]
+            arr[j], arr[j+1] = arr[j+1], arr[j]
 
 # Print the sorted array
 print(arr)
+
+# Bubble Sort Logic:
+# for i <- 1 to indexOfLastUnsortedElement-1
+#     if leftElement > rightElement
+#       swap leftElement and rightElement
+# end bubbleSort
